307ba19ab5b9a572cf63f27c61c8b22b787e6a49
aimon7 <air.ioannis@gmail.com>
2021-04-17 13:23:59 +0300

    Assignement 2

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,31 @@
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
+import { useState } from 'react';
 
-function Expenses(props){
+function Expenses(props) {
+    const initialState = {
+        year: '2021'
+    };
+    const [state, setState] = useState(initialState);
+
+    function onYearChange(year){
+        setState((prevState) => {
+            return {
+                ...prevState,
+                year: year,
+            };
+        });
+    };
+
     const expenseItems = props.expenses.map((item, index) => {
         return <ExpenseItem key={index} title={item.title} amount={item.amount} date={item.date}/>
-    })
+    });
 
     return (
         <Card className="expenses">
+            <ExpensesFilter defaultValue={state.year} onChangeYear={onYearChange}/>
             {expenseItems}
         </Card>
     )
